9e06fdb596cad9f9d9d639d8ffe3d23a75f86476
Raphael França Simonetti <120769545+CodeNetti@users.noreply.github.com>
2024-06-26 08:20:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 from PIL import Image
 import traceback
 
-# Defino o caminho da imagem
+
 
 caminho_temp = r"C:/Users/rsimonetti/Documents/ProjetoThi/Imagens/testetemp.jpg"
 caminho_pasta = r"C:/Users/rsimonetti/Documents/ProjetoThi/Imagens"
@@ -12,16 +12,16 @@ caminho_pasta = r"C:/Users/rsimonetti/Documents/ProjetoThi/Imagens"
 documento = Document()
 
 contador = 0
-# Verifique se o arquivo existe
+
 try:     
         for fotos in os.listdir(caminho_pasta):
             if fotos.endswith(('.jpg')):
                 img = os.path.join(caminho_pasta, fotos)
                 with Image.open(img) as image:
-                    image.verify()  # Verifica a integridade da imagem
-                   # print("A imagem foi verificada com sucesso.")
+                    image.verify()  # Verifico a integridade da imagem
+                  
                 with Image.open(img) as image:
-                    image.save(caminho_temp)  # Verifica a integridade da imagem
+                    image.save(caminho_temp)  
                     
                     
                 print("Inserindo a imagem no documento.")
@@ -36,14 +36,14 @@ try:
 
       
         
-        # Insiro a imagem no documento
+       
         
     
-        # Salve o documento
+        # Salvo o documento
         documento.save('Carta.docx')
         print("Imagem inseridas e documento salvo com sucesso.")
         
-        # Remova o arquivo temporário
+        # Removo o arquivo temporário
         os.remove(caminho_temp)
 
 except Exception as e:
