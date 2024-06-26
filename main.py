8ca46132fa8305f076b230d1d55155f87188b3eb
CodeNetti <raphasimonettti@gmail.com>
2024-06-26 05:39:22 -0300

    Inserindo Imagens em larga escala

--- main.py
+++ main.py
@@ -5,37 +5,47 @@ from PIL import Image
 import traceback
 
 # Defino o caminho da imagem
-caminho_imagem = r"C:/Users/rsimonetti/Pictures/teste.jpeg"
-caminho_temp = r"C:/Users/rsimonetti/Pictures/testetemp.jpeg"
 
+caminho_temp = r"C:/Users/rsimonetti/Documents/ProjetoThi/Imagens/testetemp.jpg"
+caminho_pasta = r"C:/Users/rsimonetti/Documents/ProjetoThi/Imagens"
+
+documento = Document()
+
+contador = 0
 # Verifique se o arquivo existe
-if os.path.isfile(caminho_imagem):
-    try:
-        # Verifique se a imagem pode ser aberta com Pillow
-        with Image.open(caminho_imagem) as img:
-            img.verify()  # Verifica a integridade da imagem
-            print("A imagem foi verificada com sucesso.")
+try:     
+        for fotos in os.listdir(caminho_pasta):
+            if fotos.endswith(('.jpg')):
+                img = os.path.join(caminho_pasta, fotos)
+                with Image.open(img) as image:
+                    image.verify()  # Verifica a integridade da imagem
+                   # print("A imagem foi verificada com sucesso.")
+                with Image.open(img) as image:
+                    image.save(caminho_temp)  # Verifica a integridade da imagem
+                    
+                    
+                print("Inserindo a imagem no documento.")
+                documento.add_picture(caminho_temp, width=Inches(4))
+                contador += 1
+                print(contador)
+                
+                    
+
         
-        # Abror e salvor a imagem temporariamente com Pillow
-        with Image.open(caminho_imagem) as img:
-            img.save(caminho_temp)
-            print("Imagem salva temporariamente.")
-        
-        documento = Document()
+                
+
+      
         
         # Insiro a imagem no documento
-        print("Inserindo a imagem no documento.")
-        documento.add_picture(caminho_temp, width=Inches(4))
         
+    
         # Salve o documento
         documento.save('Carta.docx')
-        print("Imagem inserida e documento salvo com sucesso.")
+        print("Imagem inseridas e documento salvo com sucesso.")
         
         # Remova o arquivo temporário
         os.remove(caminho_temp)
 
-    except Exception as e:
+except Exception as e:
         print(f"Erro ao processar o arquivo de imagem: {e}")
         print(traceback.format_exc())
-else:
-    print(f"O arquivo {caminho_imagem} não existe.")
